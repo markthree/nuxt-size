c98a4275d2c6a9c3966632f235232a5215698b1b
markthree <1801982702@qq.com>
2023-02-28 13:36:42 +0800

    feat: add buildDir size

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { cyan, gray } from "kolorist";
 // @ts-ignore
 import { getFolderSizeBin } from "go-get-folder-size/npm/bin.mjs";
@@ -5,7 +6,12 @@ import { defineNuxtModule, logger as _logger } from "@nuxt/kit";
 
 const logger = _logger.withTag("nuxt-size");
 
-export default defineNuxtModule({
+interface ModuleOptions {
+  showOutput: boolean;
+  showBuildDir: boolean;
+}
+
+export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: "nuxt-size",
     configKey: "nuxt-size",
@@ -13,18 +19,37 @@ export default defineNuxtModule({
       nuxt: "^3.0.0",
     },
   },
+  defaults: {
+    showOutput: true,
+    showBuildDir: false,
+  },
   setup(options, nuxt) {
-    const { dev, rootDir } = nuxt.options;
+    const { dev, rootDir, buildDir } = nuxt.options;
     if (dev) return;
 
-    const output = `${rootDir}/.output`;
-    nuxt.hook("close", async () => {
-      const size = await getFolderSizeBin(output, true);
+    const { showBuildDir, showOutput } = options;
 
-      console.log("\n");
-      logger.log(
-        `${cyan("Σ Output size")}: ${size}${gray(" - " + output)}\n`,
-      );
-    });
+    if (showOutput) {
+      useBuildClose(`${rootDir}/.output`, "Σ Output size");
+    }
+
+    if (showBuildDir) {
+      useBuildClose(buildDir, "Σ BuildDir size");
+    }
+
+    function useBuildClose(base: string, tip: string) {
+      nuxt.hook("build:done", () => {
+        nuxt.hook("close", async () => {
+          if (!existsSync(base)) {
+            logger.warn(`${base} does not exist`);
+            return;
+          }
+          const size = await getFolderSizeBin(base, true);
+          logger.log(
+            `${cyan(tip)}: ${size}${gray(" - " + base)}`,
+          );
+        });
+      });
+    }
   },
 });
